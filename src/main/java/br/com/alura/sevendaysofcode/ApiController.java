7d987e4abf19706827c887320baeccad9cbfd0ff
Wesley M <wesleymdeandrade@gmail.com>
2022-10-05 21:17:25 -0300

    Day 5

---
+++ src/main/java/br/com/alura/sevendaysofcode/ApiController.java
@@ -0,0 +1,18 @@
+package br.com.alura.sevendaysofcode;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ApiController {
+
+    @Autowired
+    private ImdbClient imdbClient;
+
+    @GetMapping("/api/")
+    public Wrapper<Anime> get50Animes(Model model) {
+        return imdbClient.get50Animes();
+    }
+}
--- src/main/java/br/com/alura/sevendaysofcode/ViewController.java
+++ src/main/java/br/com/alura/sevendaysofcode/ViewController.java
@@ -6,7 +6,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
-public class HtmlController {
+public class ViewController {
 
     @Autowired
     private ImdbClient imdbClient;
